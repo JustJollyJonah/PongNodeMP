dab7f6ec8eaa5f346df117532ed5336bb12607a2
Matthaia <matthias97@live.nl>
2017-03-31 22:41:13 +0200

    inlog
    Inlog gemaakt en begin client deel

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -2,7 +2,7 @@
  * Created by jonah on 23-Mar-17.
  */
 var localgame;
-$(document).on("click", "#join",function () {
+$(document).on("click", "#myBtn",function () {
     console.log("sent join");
     var username = $('input[name="uname"]').val();
     var roomnumber = parseInt($('input[name="roomnumber"]').val());
@@ -14,14 +14,53 @@ $(document).on("click", "#join",function () {
     console.log(msg);
 });
 io.on('login', function(data) {
-    console.log("Sucessfully loged in as "+data.status);
+    console.log("Sucessfully logged in as " + data.status);
     localgame = data.game;
     console.log(localgame);
 
 });
 
 $(document).on("click", "#start", function() {
-    io.emit("start", localgame.roomnumber);
-
-
+    io.emit("start", localgame);
 });
+
+
+function Setup() {
+    console.log("setup gerund");
+    $(document).on("click", "#myBtn", function(){
+        console.log("button geklikt");
+        var nameValue = document.getElementById("uname").value;
+        var roomNumber = document.getElementById("roomnumber").value;
+        console.log(roomNumber);
+        if(nameValue.length == 0){
+            $('#nameError').html("Please enter a valid name ");
+            nameValue = null;
+        }else if(roomNumber.length == 0){
+            $('#nameError').html("Please enter a valid number");
+            roomNumber = 0;
+        }else {
+            //do handling of playername
+            $(".modal").css("display","none");
+            fillRooms();
+        }
+
+
+    });
+
+}
+
+
+//rooms maken
+var roomArray = ["Awesome Room", "Less Awesome room", "Stijn's gay paradise"];
+
+function fillRooms() {
+    var rooms = document.getElementById("rooms");
+    rooms.innerHTML += "<ul>";
+    for (i = 0; i < roomArray.length; i++) {
+        var div = document.createElement('div');
+        div.innerHTML = "<li>" + roomArray[i] + "</li><BR>  ";
+        rooms.appendChild(div);
+    }
+    rooms.innerHTML += "</ul>";
+
+}
