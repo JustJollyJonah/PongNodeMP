8d80a4d95b43f682bc61ebfd4166d02cff01bd14
Matthaia <matthias97@live.nl>
2017-03-31 23:36:19 +0200

    updated pong shizzle

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -41,7 +41,7 @@ function Setup() {
         }else {
             //do handling of playername
             $(".modal").css("display","none");
-            fillRooms();
+            //fillRooms();
             $("body").css("background-color","white");
 
             console.log("sent join");
@@ -74,4 +74,40 @@ function fillRooms() {
     }
     rooms.innerHTML += "</ul>";
 
+}
+
+
+//pong
+
+/**
+ * Created by Matthaia on 31-3-2017.
+ */
+function Ball(x, y, velx, vely){
+    this.x = x;
+    this.y = y;
+    this.velx = velx;
+    this.vely = vely;
+    this.ballGrootte = 10;
+
+    this.draw = function(canvas){
+        canvas.beginPath();
+        canvas.arc(this.x, this.y, this.ballGrootte, 0, 2*Math.PI, false);
+        canvas.fillStyle = 'white';
+        canvas.fill();
+        canvas.stroke();
+    }
+}
+
+function Plank(x, y, hoogte){
+    this.x = x;
+    this.y = y;
+    this.hoogte = hoogte;
+
+    this.draw = function(canvas){
+        canvas.beginPath();
+        canvas.rect(this.x, this.y,10,this.hoogte);
+        canvas.fillStyle = 'white';
+        canvas.fill();
+        canvas.stroke();
+    }
 }
